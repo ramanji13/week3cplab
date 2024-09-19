f1487f76e641032b287b7220b18ac897c67b6b40
ramanji13 <venkataramanjireddy13@gmail.com>
2024-09-19 16:01:01 +0530

    Add files via upload

---
+++ sqrt.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a,b;
+    float c;
+    printf("enter the value of a\n");
+    scanf("%d",&a);
+    printf("\nenter the value of b");
+    scanf("%d",&b);
+    c=sqrt(a*b);
+    printf("the value of c is =%f",c);
+    }
