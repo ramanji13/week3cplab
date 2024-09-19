f7489cd57ba6bf3677207bb271f4e200fa6deddd
ramanji13 <venkataramanjireddy13@gmail.com>
2024-09-19 16:13:01 +0530

    Add files via upload

---
+++ dis.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+  int s,d,t;
+  printf("enter the speed\n");
+  scanf("%d",&s);
+  printf("enter the time\n");
+  scanf("%d",&t);
+  d=s*t;
+  printf("the distance covered =%d\n",d);
+}
